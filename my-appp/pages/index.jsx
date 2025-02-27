8cf7da2dfe1d1292577d88e9ea7777d1a79e54d9
Anna Beatriz <anna.b.valentim@aluno.senai.br>
2025-02-27 08:56:38 -0300

    update

--- my-appp/pages/index.jsx
+++ my-appp/pages/index.jsx
@@ -1,7 +1,7 @@
 import styles from "../styles/Home.module.css";
 import Header from "../componentes/Header";
 import Footer from "../componentes/Footer";
-import Main from "../componentes/main";
+import Main from "../componentes/Main";
 
 export default function Home() {
     return (
